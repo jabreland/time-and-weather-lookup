49e8e44c0e07f3d33ff3a2b8ae6fd62b5e3796aa
JABreland <brelandjohn@gmail.com>
2020-06-01 14:27:01 +0400

    Add test data from Google Geocode api

---
+++ tests/googleData.ts
@@ -0,0 +1,99 @@
+export const postalCodeA1A1A1 = {
+    results: [
+        {
+            access_points: [],
+            address_components: ['Array'],
+            formatted_address: "St. John's, NL A1A 1A1, Canada",
+            geometry: {
+                bounds: {
+                    northeast: {lat: 47.571081, lng: -52.69360289999999},
+                    southwest: {lat: 47.5692721, lng: -52.6969515}
+                },
+                location: {lat: 47.5699075, lng: -52.6954629},
+                location_type: 'APPROXIMATE',
+                viewport: {
+                    northeast: {lat: 47.5715255302915, lng: -52.69360289999999},
+                    southwest: {lat: 47.5688275697085, lng: -52.6969515}
+                }
+            },
+            place_id: 'ChIJa9jsjMajDEsRyPOIaQxwCzY',
+            types: ['Array']
+        }
+    ],
+    status: 'OK'
+}
+
+export const zip90210 =
+    {
+        results: [
+            {
+                access_points: ['Array'],
+                address_components: ['Array'],
+                formatted_address: '450 N Roxbury Dr #600, Beverly Hills, CA 90210, USA',
+                geometry: {
+                    location: {lat: 34.0683231, lng: -118.4069666},
+                    location_type: 'ROOFTOP',
+                    viewport: {
+                        northeast: {lat: 34.0696720802915, lng: -118.4056176197085},
+                        southwest: {lat: 34.0669741197085, lng: -118.4083155802915}
+                    },
+                },
+                place_id: 'ChIJ0X23Lgi8woARbiCgULcWgqQ',
+                plus_code: ['Object'],
+                types: ['Array']
+            },
+        ],
+        status: 'OK'
+    }
+
+export const toronto =
+    {
+        results: [
+            {
+                access_points: ['Array'],
+                address_components: ['Array'],
+                formatted_address: 'Toronto, ON, Canada',
+                geometry: {
+                    bounds: {
+                        northeast: {lat: 43.8554579, lng: -79.1168971},
+                        southwest: {lat: 43.5810245, lng: -79.639219}
+                    },
+                    location: {lat: 43.653226, lng: -79.3831843},
+                    location_type: 'APPROXIMATE',
+                    viewport: {
+                        northeast: {lat: 43.8554579, lng: -79.1168971},
+                        southwest: {lat: 43.5810245, lng: -79.639219}
+                    }
+                },
+                place_id: 'ChIJpTvG15DL1IkRd8S0KlBVNTI',
+                types: [Array]
+            }
+        ],
+        status: 'OK'
+    }
+
+export const newYork =
+    {
+        results: [
+            {
+                access_points: ['Array'],
+                address_components: ['Array'],
+                formatted_address: 'New York, NY, USA',
+                geometry: {
+                    bounds: {
+                        northeast: {lat: 40.9175771, lng: -73.70027209999999},
+                        southwest: {lat: 40.4773991, lng: -74.25908989999999}
+                    },
+                    location: {lat: 40.7127753, lng: -74.0059728},
+                    location_type: 'APPROXIMATE',
+                    viewport: {
+                        northeast: {lat: 40.9175771, lng: -73.70027209999999},
+                        southwest: {lat: 40.4773991, lng: -74.25908989999999}
+                    }
+                },
+                place_id: 'ChIJOwg_06VPwokRYv534QaPC8g',
+                types: ['Array']
+            }
+        ],
+        status: 'OK'
+    }
