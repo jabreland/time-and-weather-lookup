904f860dee2dd688463508cf9b11e9e423da18fe
JABreland <brelandjohn@gmail.com>
2020-06-01 13:22:43 +0400

    Process args terminal input into something that google geocode api can consume

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
 
 export const sliceOffFirstTwoElements = (args : string[]) : string [] => args.slice(2)
+export const combineArgListIntoString = (list : string[]) : string =>list.join(" ")
+export const separatePlacesInToListByCommas =
+    (places : string) :string[] => {
+        const placeNames = places.split(",")
+            .filter(name => name.length >0)
+            return placeNames.map(name => name.trim())
+    }
+export const processImportArgsIntoListOfNames = (args : string[]) : string[] => {
+    const listFromArgs = sliceOffFirstTwoElements(args)
+    const stringOfPlaceNames = combineArgListIntoString(listFromArgs)
+    return separatePlacesInToListByCommas(stringOfPlaceNames)
+}
