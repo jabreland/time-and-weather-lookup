70d3e377f0cab8bdecfafd2ca5a55a800464bfbf
JABreland <brelandjohn@gmail.com>
2020-06-03 15:41:50 +0400

    Alter the eslint, prettier and tsonfig settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,13 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['prettier', '@invisible'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 6,
+    ecmaVersion: 2020,
   },
 }
